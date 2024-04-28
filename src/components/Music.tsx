30f64f74f98ff0a6f5c42685d1d15fe7c0362060
Mayank Sahai <mayank.ram22@gmail.com>
2024-04-28 19:50:27 +0530

    added spotify music iframe for music section

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,20 +1,25 @@
-import inprogresSticker from "../assets/inprogress.png";
+import CDvol1 from "../assets/CDvol1.png";
 export function Music() {
-    
-    return (
-      <div className="flex flex-col justify-center items-center">
-        <div className="base-1 w-60 pt-20">
-          <img
-            className="rounded-full overflow-hidden shadow object-cover"
-            src={inprogresSticker}
-            alt="In progress"
-          />
-        </div>
-        <div className="base-1 flex flex-col items-center p-10 h-2/4 min-w-96 max-w-2xl">
-          <p className="font-nohemi text-center text-base sm:text-lg md:text-2xl font-semibold pb-2">
-            Work in Progress 🏗️
-          </p>
-        </div>
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="pt-32">
+      <p className="font-nohemi text-center text-base sm:text-lg md:text-2xl font-semibold pb-2">I make beats under the name MynkBeats! 🎧</p>
+      <p className="font-nohemi text-center text-base sm:text-lg md:text-2xl font-semibold pb-2">Just started learning but do checkout my album</p>
+      <p className="font-nohemi text-center text-base sm:text-lg md:text-2xl font-semibold pb-2 italic">Chaotic Disparity Vol.1 🔥</p>
       </div>
-    );
-  }
+      
+      <div className="pt-10">
+        <iframe
+          className="rounded-xl"
+          src="https://open.spotify.com/embed/artist/5JW4GnMriNzlqMweRSIyRU?utm_source=generator&theme=0"
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowfullscreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
